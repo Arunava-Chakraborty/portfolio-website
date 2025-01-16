ade079eb45f15c4b3d0f175fddb2646cb0a9aba9
Arunava Chakraborty <118994880+Arunava-Chakraborty@users.noreply.github.com>
2025-01-17 01:01:03 +0530

    Update

--- Portfolio.py
+++ Portfolio.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 # Set up the main structure of the portfolio
 st.set_page_config(page_title="My Portfolio", layout="wide")
-
+'''''
 # Sidebar for navigation
 def styled_sidebar():
     # Add custom CSS for styling the sidebar
@@ -71,7 +71,7 @@ def styled_sidebar():
 
     return clicked_button or "Home"  # Default to Home if no button is clicked
 
-
+'''''
 
 # Header Section with manual animation
 def animated_text(lines, speed=0.05):
@@ -350,7 +350,7 @@ def contact_section():
             st.write("No messages received yet.")
 
 # Render selected section
-
+'''''
 choice = styled_sidebar()
 if choice == "Home":
     header_section()
@@ -363,4 +363,9 @@ elif choice == "Contact":
 
 # Footer
 st.markdown("---")
+'''
 
+header_section()
+about_section()
+projects_section()
+contact_section()
